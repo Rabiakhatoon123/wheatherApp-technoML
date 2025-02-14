1e01a943bae5bc03dd41ab6a1cd87c6380ce829f
Rabia khatoon <rabiak73008@gmail.com>
2025-02-14 14:06:10 +0530

    weatherApp.js

--- weatherApp.js
+++ weatherApp.js
@@ -7,7 +7,7 @@ const airQualityStat = document.querySelector(".air-quality-status")
 const srchBtn = document.querySelector(".search-btn")
 const componentsEle = document.querySelectorAll(".component-val")
 
-const appId = "c400fbff4d18bd557d015e0acf532bb7" // Get your own API Key from https://home.openweathermap.org/api_keys
+const appId = "" // Get your own API Key from https://home.openweathermap.org/api_keys
 const link = "https://api.openweathermap.org/data/2.5/air_pollution"	// API end point
 
 const getUserLocation = () => {
